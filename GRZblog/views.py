13ed101a18a605a3893d09acace5850b4d22bdf4
grazieragazzi <gritskevichvlad@gmail.com>
2020-07-27 17:47:13 -0400

    Good time to learn how templates work!

--- GRZblog/views.py
+++ GRZblog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
+
+posts = [
+	{
+		'author': 'grazieragazzi',
+		'title': 'OpenKeys writeup',
+		'content': 'My first writeup ever',
+		'date_posted': 'July 27, 2020'
+	},
+	{
+		'author': 'mikasa<3',
+		'title': 'Buff writeup',
+		'content': 'My second writeup',
+		'date_posted': 'July 28, 2020'
+	}
+]
+
 def home(request):
-	return HttpResponse('<h1>Blog Home</h1>')
+	context = {
+		'posts': posts
+	}
+	return render(request, 'GRZblog/home.html', context)
 
 def about(request):
-	return HttpResponse('<h1>Blog About</h1>')
+	return render(request, 'GRZblog/about.html', {'title': 'GRZblog'})
--- new_blog/settings.py
+++ new_blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'GRZblog.apps.GrzblogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
